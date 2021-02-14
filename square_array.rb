c3de160d959f9a8c323c8234d04195a04e02ec4c
Omar Gonzalez <ogonzaleznyc@gmail.com>
2021-02-14 00:27:46 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,15 @@
+# def square_array(array)
+#   squared_array = []
+#   array.each do |element|
+#     squared_array << element * element
+#   end
+#   squared_array
+# end
+
 def square_array(array)
   squared_array = []
-  array.each do |element|
-    squared_array << element * element
+  array.each do |e|
+    squared_array << e * e 
   end
   squared_array
 end
